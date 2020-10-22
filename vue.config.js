7d422eb0c19a67272c02657188c4fe9879e0fc50
pengtianfu <peng7800809@163.com>
2020-10-22 15:08:06 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -6,25 +6,20 @@ module.exports = {
 			    // host:'http://192.168.3.18',
 			　　port : 8080, //端口号
 			　　proxy : {
-			// 　　　　"/dev-api" : {
-			// 　　　　"target" : "192.168.3.5:9007", //目标接口域名
-			// 　　　　"changeOrigin" : true, //是否跨域
-			// 　　　　"secure" : false // 设置支持https协议的代理
-			// 　　　　 },
 			       "/user" : {
 			　　　　 "target" : "http://192.168.3.5:9007", //目标接口域名
 			　　　　 "changeOrigin" : true, //是否跨域
 			　　　　  "secure" : false // 设置支持https协议的代理
 			　　　　 },
 				   "/captchaImage" : {
-			　　　　 "target" : "http://192.168.3.5:9007", //目标接口域名
-			　　　　 "changeOrigin" : true, //是否跨域
-			　　　　  "secure" : false // 设置支持https协议的代理
+			　　　　 "target" : "http://192.168.3.5:9007", 
+			　　　　 "changeOrigin" : true, 
+			　　　　  "secure" : false 
 			　　　　 },
 			     "/system" : {
-			　　　　 "target" : "http://192.168.3.5:9007", //目标接口域名
-			　　　　 "changeOrigin" : true, //是否跨域
-			　　　　  "secure" : false // 设置支持https协议的代理
+			　　　　 "target" : "http://192.168.3.5:9007", 
+			　　　　 "changeOrigin" : true, 
+			　　　　  "secure" : false 
 			　　　　 },
 			　　　　}
 			　　}
