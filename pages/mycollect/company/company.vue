505c4a75b40198b85a0b1dfa3c89b35d05aee1ee
pengtianfu <peng7800809@163.com>
2020-10-27 14:17:26 +0800

    优化

--- pages/mycollect/company/company.vue
+++ pages/mycollect/company/company.vue
@@ -10,27 +10,24 @@
 		<swiper class="swiper-box" :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish">
 			<swiper-item class="swiper-item">
 				<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="onreachBottom">
-					<view class="scroll-item" @click="toView()" v-for="itm in 10" :key='itm'>
-						<u-row>
-							<u-col span="12" class="time">
-								<view style="padding-right: 10pt;">上海</view>
-							</u-col>
-							<u-col span="5" @click="toView()">
-								<view><image :src="goodsUrl" mode="aspectFill"></image></view>
-							</u-col>
-							<u-col span="6" class="border-left" @click="toView()">
-								<view class="title u-line-2">艾瑞泽52020款 1.5L CVT运动版CVT运动版</view>
-								<view class="type"><text>打包价</text><text class="type-money">￥2700</text></view>
-								<view><text>纯电动</text><text>Suv</text><text>自动挡</text></view>
-							</u-col>
-							<u-col span="12" class="bottom">
-								<text class="bottom-left" >车龄≤3个月</text>
-								<text class="bottom-right">车龄≤3个月</text>
-								<u-icon name="heart-fill" color="#3FB26C" size="36"></u-icon>
-							</u-col>
-						</u-row>
+					<view class="scroll-item" @click="toView()" v-for="itm in 5" :key='itm'>
+						<view class="time">
+							<view class="padding15">上海</view>
+						</view>
+						<view class="img" @click="toView()">
+							<view><image :src="goodsUrl" mode="aspectFill"></image></view>
+						</view>
+						<view class="border-left" @click="toView()">
+							<view class="title u-line-2">艾瑞泽52020款 1.5L CVT运动版CVT运动版</view>
+							<view class="type"><text>打包价</text><text class="type-money">￥2700</text></view>
+							<view><text>纯电动</text><text>Suv</text><text>自动挡</text></view>
+						</view>
+						<view class="bottom">
+							<text class="bottom-left" >车龄≤3个月</text>
+							<text class="bottom-right">车龄≤3个月</text>
+							<u-icon name="heart-fill" color="#3FB26C" size="36"></u-icon>
+						</view>
 					</view>
-					
 							<!-- <u-loadmore :status="loadStatus[0]" bgColor="#f2f2f2"></u-loadmore> -->
 				</scroll-view>
 			</swiper-item>
@@ -38,26 +35,22 @@
 			<swiper-item class="swiper-item">
 				<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="onreachBottom">
 					<view class="scroll-item" @click="toView()" v-for="item in 7" :key='item'>
-						<u-row class="" style="padding: 8pt 15pt;">
-							<u-col span="11" style="padding: 5pt 0;">
-								<view>刷新时间：刚刚</view>
-							</u-col>
-							<u-col span="1" style="padding: 5pt 0;">
+						<view class="zplist">
+							<view style="padding: 5pt 0;">
+								<text class="zplist-time">刷新时间：刚刚</text>
 								<u-icon name="heart-fill" color="#36AB62" size="40"></u-icon>
-							</u-col>
-							<u-col span="12" @click="toView()" style="border-top: 1px solid #E5E5E5;padding-top: 8pt;">
-								<view style="font-size: 14pt;font-weight: bold;">求购：30辆 荣威\吉利帝豪\比亚迪...</view>
+							</view>
+							<view class="zplist-carName">
+								<view class="zplist-qg">求购：30辆 荣威\吉利帝豪\比亚迪...</view>
 								<view class="">
 									<text>金华诚信租车行     金华</text>
 								</view>
 								<view>
-									<text style="">打包价：</text><text style="color: #3FB26C;font-size: 18pt;">3万以内/3-5万</text>
+									<text>打包价：</text><text class="zplist-price">3万以内/3-5万</text>
 								</view>
-							</u-col>
-						</u-row>
-						<view class="bottom">
-							 
+							</view>
 						</view>
+    
 					    		
 					</view>
 							<!-- <u-loadmore :status="loadStatus[0]" bgColor="#f2f2f2"></u-loadmore> -->
@@ -105,7 +98,7 @@
 			},
 			// scroll-view到底部加载更多
 			onreachBottom() {
-				
+				console.log(this.$attrs)
 			}
 		}
 	}
@@ -124,11 +117,11 @@ page{
 	}
 </style>
 <style lang="scss" scoped>
-	.driver-content{
-		.u-tabs{
-			background: linear-gradient(115deg,$bg-grad-FE, $bg-grad-FCD);
-		}
-	}
+	// .driver-content{
+	// 	.u-tabs{
+	// 		background: linear-gradient(115deg,$bg-grad-FE, $bg-grad-FCD);
+	// 	}
+	// }
 	.company-content{
 		.u-tabs{
 			background: linear-gradient(115deg, $bg-grad-AB, $bg-grad-DDC);
@@ -152,7 +145,13 @@ page{
 	box-shadow: 0px 0px 18px 0px rgba(69, 183, 114, 0.24);
     .time{
 		font-weight: bold;
-		text-align: right!important;
+		text-align: right;
+	}
+	.padding15{
+		padding-right: 15pt;
+	}
+	.img{
+		display: inline-block;width:40% 
 	}
 	image {
 		width: 200rpx;
@@ -161,7 +160,8 @@ page{
 	}
 	.border-left{
 		border-left: 1px solid #7F7F7F;
-		padding-left: 30rpx!important;
+		padding-left: 30rpx;
+		display: inline-block;width:55% ;
 	}
 	.title{
 		font-weight: bold;
@@ -175,7 +175,7 @@ page{
 	}
 	.bottom{
 		background-color: #CDE5E3;
-		padding: 5px!important;
+		padding: 5px;
 		border-bottom-right-radius: 40rpx;
 		border-bottom-left-radius: 40rpx;
 	}
@@ -193,6 +193,21 @@ page{
 		font-size: 10pt;
 		width: 50%;
 		display: inline-block;
+	}
+	.zplist{
+		padding: 8pt 10pt;
+	}
+	.zplist-time{
+		width: 90%;display: inline-block;
+	}
+	.zplist-carName{
+		border-top: 1px solid #E5E5E5;padding-top: 8pt;
+	}
+	.zplist-qg{
+		font-size: 14pt;font-weight: bold;
+	}
+	.zplist-price{
+		color: #3FB26C;font-size: 18pt;
 	}
 }
 
