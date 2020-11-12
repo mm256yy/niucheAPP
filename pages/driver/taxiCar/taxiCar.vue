eb148078e4addbb4b54da208d014a0b20095ba2f
pengtianfu <peng7800809@163.com>
2020-11-12 21:17:57 +0800

    123

--- pages/driver/taxiCar/taxiCar.vue
+++ pages/driver/taxiCar/taxiCar.vue
@@ -100,7 +100,7 @@
 				fileList: [],
 				radioList:[{name:'0',text:'女'},{name:'1',text:'男'}],
 				form: {
-					state:1,
+					type:1,
 					name:'',
 					sex: '',
 					licenseNumber:'',
@@ -146,7 +146,7 @@
 		},
 		methods: {
 			getInfo(){
-				this.$u.api.listDrivingLicense({type:1}).then(res => {
+				this.$u.api.listDrivingLicense({state:1}).then(res => {
 					if(res.code === 200){
 						let data = res.object;
 						this.form =data;
