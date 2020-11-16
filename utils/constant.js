6f67cf5b06391507472f3b851d8e973fd298899a
pengtianfu <peng7800809@163.com>
2020-11-16 15:33:27 +0800

    12

--- utils/constant.js
+++ utils/constant.js
@@ -1,13 +1,6 @@
 
-let ipconfig = ''
-if(process.env.NODE_ENV === 'development'){
-//     console.log('开发环境')
-	ipconfig = 'http://192.168.3.5:9007'
-}else{
-let	ipconfig = 'http://server.neocab.cn'
-    // console.log('生产环境')
-}
-
+let ipconfig = 'http://server.neocab.cn'
 export const action =ipconfig+ "/sys/upload/uploadImage";
 export const actionJx =ipconfig+ "/user/image/BusinessImagedemo";
 export const actionJs = ipconfig+'/user/image/DriverCardLicense';
+export const baseUrl = ipconfig;
