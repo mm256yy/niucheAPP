eac8f213616dfc6e33d5576ca835ce0f1a320cdf
pengtianfu <peng7800809@163.com>
2020-11-27 21:21:23 +0800

    更新

--- App.vue
+++ App.vue
@@ -1,8 +1,41 @@
+
 <script>
 		import {dirverPages,companyPages} from '@/utils/tabbar.js'
 	export default {
 		onLaunch: function() {
-			
+			// #ifdef APP-PLUS
+			 plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) { 
+			    uni.request({  
+			        url: 'http://app-server.neocab.cn/app/getUrl?version='+widgetInfo.versionCode, 
+					method:'POST',
+			        data: {  
+			            version: widgetInfo.versionCode,  
+			        },  
+			        success: (res) => {
+						if(res.data.code === 200){
+						let data = res.data.object;
+						if(data.checkState === 1 ){
+							  uni.downloadFile({  
+								url: data.url,  
+								success: (downloadResult) => {  
+									if (downloadResult.statusCode === 200) {  
+										plus.runtime.install(downloadResult.tempFilePath, {  
+											force: false  
+										}, function() {  
+											console.log('install success...');  
+											plus.runtime.restart();  
+										}, function(e) {  
+											console.error('install fail...');  
+										});  
+									}  
+								}  
+							});  
+						} 
+					} 
+			     },
+				})
+			});  
+			// #endif
 				let date = new Date();
 				let year = date.getFullYear();
 				let month = date.getMonth()+1;
@@ -36,6 +69,8 @@
 					   });
 				   }
 			})
+			
+
 		},
 		onShow() {
 			let type = uni.getStorageSync('curThemeType');
