a074fcd426065aae17da26a1a97862c250a4028b
sudongling <18749412671@163.com>
2020-12-02 16:08:26 +0800

    1

--- pages/index/driver/components/index/rangePrice.vue
+++ pages/index/driver/components/index/rangePrice.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="range-price">
 		<view class="padding">
-			<v-tabs v-model="firstCurrent" :scroll="true" lineHeight='0rpx' :pills="true" pillsColor="#FF6501" pillsBorderRadius="0rpx"
-			 inactive-color="#000000" activeColor="#ffffff" :tabs="tab" @change="change"></v-tabs>
+			<v-tabs v-model="firstCurrent" :scroll="true" lineHeight='0rpx' :pills="true" pillsColor="#ffffff" pillsBorderRadius="0rpx"
+			 inactive-color="#000000" activeColor="#FF6501" :tabs="tab" @change="change"></v-tabs>
 			 <view v-for="(item, index) in detail.carRentPriceCollection" :key="index" class="" v-show="firstCurrent === index">
 			    <view class="bg">
 			        <view class="year">车龄：≤{{detail.carage}}年</view>
