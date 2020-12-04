2d0d321cd478c535e377a86004fa09c1dae0b8dd
pengtianfu <peng7800809@163.com>
2020-12-04 15:19:45 +0800

    优化

--- App.vue
+++ App.vue
@@ -1,100 +1,182 @@
 
 <script>
-		import {dirverPages,companyPages} from '@/utils/tabbar.js'
+	import Vue from 'vue'
+	import {dirverPages,companyPages} from '@/utils/tabbar.js'
 	export default {
 		onLaunch: function() {
-			// #ifdef APP-PLUS
-			//设置2.4秒后主动关闭，最多设置6秒
-				setTimeout(() => {
-					plus.navigator.closeSplashscreen();
-				}, 2400);
-			// #endif
-			// #ifdef APP-PLUS
-			 plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) { 
-			    uni.request({  
-			        url: 'http://app-server.neocab.cn/app/getUrl?version='+widgetInfo.versionCode, 
-					method:'POST',
-			        data: {  
-			            version: widgetInfo.versionCode,  
-			        },  
-			        success: (res) => {
-						if(res.data.code === 200){
-						let data = res.data.object;
-						if(data.checkState === 1 ){
-							  uni.downloadFile({  
-								url: data.url,  
-								success: (downloadResult) => {  
-									if (downloadResult.statusCode === 200) {  
-										plus.runtime.install(downloadResult.tempFilePath, {  
-											force: false  
-										}, function() {  
-											console.log('install success...');  
-											plus.runtime.restart();  
-										}, function(e) {  
-											console.error('install fail...');  
-										});  
-									}  
-								}  
-							});  
-						} 
-					} 
-			     },
-				})
-			});  
-			// #endif
+			//初始化
+			this.initSystem();
+			//检测升级
+			this.checkUpdate()
+			//中间图标点击
+			this.midButtonTap()
+
+		},
+		onShow() {
+			this.setTabBar()
+		},
+		methods:{
+			initSystem(){
+				// #ifdef APP-PLUS
+					setTimeout(() => {
+						plus.navigator.closeSplashscreen();
+					}, 2000);
+				// #endif
 				let date = new Date();
 				let year = date.getFullYear();
 				let month = date.getMonth()+1;
 				let day = date.getDate();
 				let obj = {year:year,month:month,day:day};
 				 uni.setStorageSync('today', obj);
-			     let type = uni.getStorageSync('curThemeType');
+				 let type = uni.getStorageSync('curThemeType');
 				 if(!type){
 					 uni.setStorageSync('curThemeType', 'driver');
 				 }
-			    uni.onTabBarMidButtonTap(function(e){
+			},
+			//检测升级
+			checkUpdate(){
+				const self = this
+				uni.getSystemInfo({
+					success: function(e) {
+						// app整包更新检测
+						self.appUpgrade(e.platform)
+					}
+				})
+				// #ifdef APP-PLUS
+				 plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) { 
+				    uni.request({  
+				        url: 'http://app-server.neocab.cn/app/getUrl?version='+widgetInfo.versionCode, 
+						method:'POST',
+				        data: {  
+				            version: widgetInfo.versionCode,  
+				        },  
+				        success: (res) => {
+							if(res.data.code === 200){
+							let data = res.data.object;
+							if(data.checkState === 1 ){
+								  uni.downloadFile({  
+									url: data.url,  
+									success: (downloadResult) => {  
+										if (downloadResult.statusCode === 200) {  
+											plus.runtime.install(downloadResult.tempFilePath, {  
+												force: false  
+											}, function() {  
+												console.log('install success...');  
+												plus.runtime.restart();  
+											}, function(e) {  
+												console.error('install fail...');  
+											});  
+										}  
+									}  
+								});  
+							} 
+						} 
+				     },//success
+					})
+				});  
+				// #endif
+			},
+			/**
+			 * app整包更新检测
+			 */
+			appUpgrade(platform) {
+				let that = this;
+				if (platform !== 'android') {
+					return
+				}
+				//#ifdef APP-PLUS
+				plus.runtime.getProperty(plus.runtime.appid, (wgtinfo) => {
+					uni.request({
+					    url: 'http://app-server.neocab.cn/app/getUrl?version='+wgtinfo.versionCode, 
+						method:'POST',
+					    data: {version: wgtinfo.versionCode},  
+					    success: (res) => {
+							if(res.data.code === 200){
+							   let data = res.data.object;
+							   if(data.checkState === 1 ){
+								  that.showUpdateModel(data)
+							   } //if
+						   } //code 200
+					 }
+				   })//请求
+				})
+				//#endif
+			},
+			showUpdateModel(data){
+				let that = this;
+				uni.showModal({
+					title: "下载更新提示",
+					content: '检测到有新的版本，请升级',
+					showCancel: false,
+					confirmText: '确定',
+					success: sucRes => {
+						if (sucRes.confirm) {
+                            that.downFile(data)
+						}
+					}//确定按钮
+				})//model
+			},
+			downFile(data){
+				uni.downloadFile({
+					url: data.url,  
+					success: (downloadResult) => {  
+						if (downloadResult.statusCode === 200) {  
+							plus.runtime.install(downloadResult.tempFilePath, {  
+								force: false  
+							}, function() {  
+								console.log('install success...');  
+								plus.runtime.restart();  
+							}, function(e) {  
+								console.error('install fail...');  
+							});  
+						}  
+					}  
+				}); //dowload
+			},
+			midButtonTap(){
+				uni.onTabBarMidButtonTap(function(e){
 				   let token = uni.getStorageSync('token');
 				   if (token){
 					   let role = uni.getStorageSync('role');
 					   console.log(role)
 					   if (role == 2){
 						  uni.navigateTo({
-						      url: '/pages/company/release/release'
+							  url: '/pages/company/release/release'
 						  });
 					   } else{
 						   uni.navigateTo({
-						       url: '/pages/driver/release/release'
+							   url: '/pages/driver/release/release'
 						   });
 					   }
 				   } else {
-					    uni.showToast({
-					        title:"请先登录，认证" 
-					      })
+						uni.showToast({
+							title:"请先登录，认证" 
+						  })
 					   uni.switchTab({
-					       url: '/pages/mycenter/mycenter'
+						   url: '/pages/mycenter/mycenter'
 					   });
 				   }
-			})
+				})
+			},
+		    setTabBar(){
+			  let type = uni.getStorageSync('curThemeType');
+			  if (type === 'company'){
+			  	companyPages.forEach(item=>{
+			  		uni.setTabBarItem(item)
+			  	})
+			  	uni.setTabBarStyle({
+			  	  selectedColor: '#41B36D',
+			  	})
+			  } else {
+			  	dirverPages.forEach(item=>{
+			  		uni.setTabBarItem(item)
+			  	})
+			  	uni.setTabBarStyle({
+			  	  selectedColor: '#FE9217',
+			  	})
+			  }
+		  },	
 			
-
-		},
-		onShow() {
-			let type = uni.getStorageSync('curThemeType');
-			if (type === 'company'){
-				companyPages.forEach(item=>{
-					uni.setTabBarItem(item)
-				})
-				uni.setTabBarStyle({
-				  selectedColor: '#41B36D',
-				})
-			} else {
-				dirverPages.forEach(item=>{
-					uni.setTabBarItem(item)
-				})
-				uni.setTabBarStyle({
-				  selectedColor: '#FE9217',
-				})
-			}
 		},
 		onHide: function() {
 			console.log('App Hide')
