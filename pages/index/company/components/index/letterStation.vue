74264a2a658d630cb5826891d28ad0e89936beea
sudongling <18749412671@163.com>
2020-12-08 19:06:42 +0800

    未读已读样式修改

--- pages/index/company/components/index/letterStation.vue
+++ pages/index/company/components/index/letterStation.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="message">
-		<u-navbar back-text="返回" back-icon-size="0" title="我的通知" :back-text-style="backTextStyle" title-width="300" height='44' title-color="#000000">
+		<u-navbar back-text="返回" back-icon-size="0" title="我的通知" title-width="300" height='44' title-color="#000000">
 		</u-navbar>
-		<view v-show="item.state==2" class="content" v-for="(item, index) in list" :key="index">
+		<view v-show="item.state==2" class="contentno" v-for="(item, index) in list" :key="index">
 			<view @click="listMessage()" class="list">
 				<view style="width: 14rpx;height: 14rpx;border-radius: 50%;background-color: #ff5644;"></view>
 				<view class="title">{{item.title}}</view>
@@ -10,9 +10,8 @@
 			</view>
 			<view class="text">{{item.content}}</view>
 		</view>
-		<view v-show="item.state==1" class="contentno" v-for="(item, index) in list" :key="index">
+		<view v-show="item.state==1" class="content" v-for="(item, index) in list" :key="index">
 			<view @click="listMessage()" class="list">
-				<view style="width: 14rpx;height: 14rpx;border-radius: 50%;background-color: #ff5644;"></view>
 				<view class="title">{{item.title}}</view>
 				<view class="time">{{item.begintime}}</view>
 			</view>
@@ -84,39 +83,6 @@
 	}
   .message{
 	  .content{
-		  width: 100%;
-		  border-radius: 30rpx;
-		  background-color: #fff;
-		  padding: 30rpx;
-		  display: flex;
-		  flex-direction: column;
-		  margin-top: 20rpx;
-		 .list{
-			 display: flex;
-			 align-items: center;
-			 margin-top: 20rpx;
-			 font-family: PingFangSC-Medium, PingFang SC;
-			 .title{ 
-				 font-size: 30rpx;
-				 font-weight: 500;
-				 color: #111111;
-				 margin-left: 12rpx;
-				 margin-right: 20rpx;
-			 }
-			 .time{
-				font-size: 24rpx;
-				font-weight: 400;
-				color: #C7C7C7; 
-			 }
-		 }
-		 .text{
-		 				font-size: 26rpx;
-		 				font-weight: 500;
-		 				color: #666666;
-		 				margin-top: 20rpx;
-		 }
-	  }
-	  .contentno{
 	  		  width: 100%;
 	  		  border-radius: 30rpx;
 	  		  background-color: #fff;
@@ -131,7 +97,7 @@
 	  			 font-family: PingFangSC-Medium, PingFang SC;
 	  			 .title{ 
 	  				 font-size: 30rpx;
-	  				 font-weight: 500;
+	  				 font-weight: 900;
 	  				 color: #666666;
 	  				 margin-left: 12rpx;
 	  				 margin-right: 20rpx;
@@ -146,6 +112,39 @@
 	  		 				font-size: 26rpx;
 	  		 				font-weight: 500;
 	  		 				color: #999999;
+	  		 				margin-top: 20rpx;
+	  		 }
+	  }
+	  .contentno{
+	  		  width: 100%;
+	  		  border-radius: 30rpx;
+	  		  background-color: #fff;
+	  		  padding: 30rpx;
+	  		  display: flex;
+	  		  flex-direction: column;
+	  		  margin-top: 20rpx;
+	  		 .list{
+	  			 display: flex;
+	  			 align-items: center;
+	  			 margin-top: 20rpx;
+	  			 font-family: PingFangSC-Medium, PingFang SC;
+	  			 .title{ 
+	  				 font-size: 30rpx;
+	  				 font-weight: 900;
+	  				 color: #111111;
+	  				 margin-left: 12rpx;
+	  				 margin-right: 20rpx;
+	  			 }
+	  			 .time{
+	  				font-size: 24rpx;
+	  				font-weight: 400;
+	  				color: #C7C7C7; 
+	  			 }
+	  		 }
+	  		 .text{
+	  		 				font-size: 26rpx;
+	  		 				font-weight: 500;
+	  		 				color: #666666;
 	  		 				margin-top: 20rpx;
 	  		 }
 	  }
