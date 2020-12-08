937ac153ab305496878b07dfd2f2e2df9ba6b98c
pengtianfu <peng7800809@163.com>
2020-12-08 17:20:58 +0800

    优化

--- pages/driver/inviteFriends/inviteFriends.vue
+++ pages/driver/inviteFriends/inviteFriends.vue
@@ -12,7 +12,7 @@
 			<view>您的邀请码</view>
 			<view>88888</view>
 			<view style="padding-top: 30pt;">
-				<u-button type="warning" shape='circle' @click="toNext" style="width: 150pt;">立即邀请好友</u-button>
+				<u-button type="warning" shape='circle' @click="inviteFriends" style="width: 150pt;">立即邀请好友</u-button>
 			</view>
 		</view>
 	</view>
@@ -30,6 +30,23 @@
 		methods: {
 			showTips(){
 				
+			},
+			inviteFriends(){
+				uni.share({
+				    provider: "weixin",
+				    scene: "WXSceneSession",
+				    type: 0,
+				    href: 'https://mp.weixinbridge.com/mp/wapredirect?url=http%3A%2F%2Fimage.neocab.cn%2F__UNI__A80C5DE_1207170912.apk',
+				    title: "纽车分享",
+				    // summary: "我正在使用HBuilderX开发uni-app，赶紧跟我一起来体验！",
+				    imageUrl: "http://image.neocab.cn/logo.png",
+				    success: function (res) {
+				        console.log("success:" + JSON.stringify(res));
+				    },
+				    fail: function (err) {
+				        console.log("fail:" + JSON.stringify(err));
+				    }
+				});
 			}
 		}
 	}
