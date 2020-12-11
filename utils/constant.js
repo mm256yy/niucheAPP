3d76fe54151f59fe91a737a6fb35600034078083
pengtianfu <peng7800809@163.com>
2020-12-11 13:36:12 +0800

    优化

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,5 @@
 
-let ipconfig = 'http://server.neocab.cn'
+let ipconfig = 'http://app-server.neocab.cn'
 export const action =ipconfig+ "/sys/upload/uploadImage";
 export const actionJx =ipconfig+ "/user/image/BusinessImagedemo";
 export const actionJs = ipconfig+'/user/image/DriverCardLicense';
