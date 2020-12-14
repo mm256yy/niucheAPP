5000281ada419b8ec6a42d544e274f69e392ab70
pengtianfu <peng7800809@163.com>
2020-12-14 18:18:53 +0800

    代码

--- utils/mixin.js
+++ utils/mixin.js
@@ -1,28 +1,30 @@
-import {mapGetters} from 'vuex'
+import {
+	mapGetters
+} from 'vuex'
 
 export default {
-   install(Vue) {
-       Vue.mixin({
-           data() {
-             return {
-				 backgroundDri: {
-				 	'background-image': 'linear-gradient(55deg, #FE9217, #FCD03C)'
-				 },
-				 backgroundCom: {
-				 	'background-image': 'linear-gradient(55deg, #37AB63, #6DD99C)'
-					// 'background-image': 'linear-gradient(55deg, #37AB63, #34b98b)'
-				 },
-                curThemeType:''
-			 }
-           },
-		   onLoad() {
+	install(Vue) {
+		Vue.mixin({
+			data() {
+				return {
+					backgroundDri: {
+						'background-image': 'linear-gradient(55deg, #FE9217, #FCD03C)'
+					},
+					backgroundCom: {
+						'background-image': 'linear-gradient(55deg, #37AB63, #6DD99C)'
+						// 'background-image': 'linear-gradient(55deg, #37AB63, #34b98b)'
+					},
+					curThemeType: ''
+				}
+			},
+			onLoad() {
 				let type = uni.getStorageSync('curThemeType');
 				this.curThemeType = type
-		   },
-		   onShow () {
+			},
+			onShow() {
 				let type = uni.getStorageSync('curThemeType');
 				this.curThemeType = type
-		   },
-       })
-   }
+			},
+		})
+	}
 }
