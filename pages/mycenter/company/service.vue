f01fc1b4d6616314c951b3e81f4b75d5620a7024
sudongling <18749412671@163.com>
2020-12-21 10:44:17 +0800

    1

---
+++ pages/mycenter/company/service.vue
@@ -0,0 +1,85 @@
+<template>
+	<view class="message">
+		<u-navbar back-text="返回" back-icon-size="0" title="纽车客户服务中心" title-width="300" height='44' title-color="#111" title-bold="true">
+		</u-navbar>
+		<view style="width: 680rpx;margin-left: 36rpx;background: #fff;border-radius: 20rpx 20rpx 0 0;margin-top: 36rpx;">
+			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;border-bottom: 2rpx solid rgba(0,0,0,0.03);">
+				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">线上客服</view>
+				<view style="border-radius: 12rpx;border: 1px solid #4ABA75;line-height: 62rpx;text-align: center;
+				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;">联系客服</view>
+			</view>
+			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;border-bottom: 2rpx solid rgba(0,0,0,0.03);">
+				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">微信号：niuchekeji</view>
+				<view style="border-radius: 12rpx;border: 1px solid #4ABA75;line-height: 62rpx;text-align: center;
+				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;">复制加友</view>
+			</view>
+			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;">
+				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">客服电话：0571-878115287</view>
+				<view @click="dial" style="border-radius: 12rpx;border: 1px solid #4ABA75;line-height: 62rpx;text-align: center;
+				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;">拨打电话</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				time:''
+			}
+		},
+		onLoad(option) {
+			let time = option.time;
+			if(time){
+			 this.time = time;
+			}
+		},
+		methods: {
+			dial(){
+				uni.makePhoneCall({
+				    phoneNumber: '0571-878115287' 
+				});
+			}
+	    },
+	}
+</script>
+<style lang="scss" scoped>
+	page{
+		background: #f5f5f5;
+	}
+  .message{
+	  .content{
+		 .list{
+		 		  margin: 0 36rpx;
+				  padding: 36rpx 0 0 0;
+		 		  font-family: PingFangSC-Regular, PingFang SC;
+		 		  display: flex;
+		 		  border-bottom: 1rpx solid #e0e0e0;
+		 		 .box{
+		 			 margin-left: 28rpx;
+		 		 		  .title{
+							     width: 340rpx;
+		 		 		  		 font-size: 32rpx;
+		 		 		  		 font-weight: 400;
+		 		 		  		 color: #333333;
+		 						  margin-bottom: 16rpx;
+		 		 		  }
+		 		 		  .join{
+		 		 		  		 font-size: 26rpx;
+		 		 		  		 font-weight: 400;
+		 		 		  		 color: #999999;
+		 						  margin-bottom: 40rpx;
+		 		 		  }
+						  .time{
+						  			  font-size: 24rpx;
+						  			  font-weight: 400;
+						  			  color: #C7C7C7;
+						  }
+		 		 } 
+		 } 
+	  }
+  }
+</style>
+
+
---
+++ pages/mymessage/company/components/index/setting.vue
@@ -0,0 +1,160 @@
+<template>
+	<view class="setting">
+		<u-navbar back-text="返回" back-icon-size="0" title="基本参数"
+		  height='44' title-color="#111"><!-- <view @click="history()" style="color: #fff;margin-right: 20rpx;font-size: 30rpx;" slot="right">
+				   历史</view> --></u-navbar>
+		<view class="content">
+			<!-- <u-form label-width="200" label-align="left" :model="detail" ref="uForm">
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="车型名称"><u-input :disabled="true" height="30" input-align="right" v-model="detail.specification" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="厂商指导价(元)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.specification" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="厂商"><u-input :disabled="true" height="30" input-align="right" v-model="detail.trunk" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="级别"><u-input :disabled="true" height="30" input-align="right" v-model="detail.wheel" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="能源类型"><u-input :disabled="true" height="30" input-align="right" v-model="detail.environmental" placeholder="暂无数据"/></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="环保标准"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="上市时间"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="最大功率(kw)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="最大扭矩"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="发动机"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="变速箱"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="长*宽*高(mm)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="车身结构"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="最高车速(km/h)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="官方0-100km/h加速(s)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="实测0-100km/h加速(s)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="实测100-0km/h制动(m)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="工信部综合油耗(L/100km)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.displacement" placeholder="暂无数据" /></u-form-item>
+			</u-form> -->
+		</view>
+		<!-- <view class="other">其他参数</view> -->
+		<!-- <view class="list"> -->
+			<!-- <u-form label-width="200" label-align="left" :model="detail" ref="uForm">
+				<u-form-item v-for="(item, index) in detail.otherparam" :key="index" style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" :label="车门个数(个)"><u-input :disabled="true" height="30" input-align="right" v-model="detail.otherparam[0]" /></u-form-item>
+				<u-form-item style="margin-left: 39rpx;margin-right: 46rpx;padding: 6rpx 0;" label="驱动方式"><u-input :disabled="true" height="30" input-align="right" v-model="detail.otherparam[1]" /></u-form-item>
+			</u-form> -->
+<!-- 			<view class="params" v-for="(item, index) in detail.otherparam" :key="index">
+				<text class="left">{{item.paramtername}}</text><text class="left">({{item.paramterunit}})</text>
+				<text class="right">{{item.paramtertext}}</text>
+				<view class="clear"></view>
+			</view> -->
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				id:''
+			}
+		},
+		onLoad(option) {
+			let id = option.id;
+			if(id){
+			 this.id = id;
+			}
+		},
+		mounted() {
+			this.getData()
+		},
+		methods: {
+			getData() {
+				this.$u.api.setting({carmodelid: this.id}).then(res=>{
+					if(res.code === 200){
+						 
+					}else {
+						 this.$u.toast(res.msg);
+					}
+				})
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+    .setting {
+		.clear {
+			clear: both;
+		}
+		.top {
+			width: 670rpx;
+			height: 84rpx;
+			background: #FFF;
+			margin-left: 40rpx;
+			margin-bottom: 33rpx;
+			padding: 9rpx 0;
+			font-size: 28rpx;
+			font-weight: 900;
+			.new {
+				width: 66rpx;
+				height: 66rpx;
+				line-height: 66rpx;
+				text-align: center;
+				font-size: 20rpx;
+				font-weight: 900;
+				color: #fff;
+				border-radius: 50%;
+				background: linear-gradient(115deg, #6DD99B, #37AB63);
+				margin-left: 40rpx;
+				float: left;
+			}
+			.year {
+				margin-left: 25rpx;
+				margin-right: 85rpx;
+				float: left;
+				margin-top: 18rpx;
+			}
+			.distance {
+				float: left;
+				margin-top: 18rpx;
+			}
+		}
+	    .content {
+			width: 670rpx;
+			// height: 485rpx;
+			background: #fff;
+			margin-left: 40rpx;
+		}
+		.other {
+			margin-left: 40rpx;
+			margin-top: 40rpx;
+			margin-bottom: 40rpx;
+			font-size: 28rpx;
+			color: #7f7f7f;
+		}
+		.list {
+			width: 670rpx;
+			// height: 162rpx;
+			background: #fff;
+			margin-left: 40rpx;
+			.params {
+				padding: 20rpx 46rpx 20rpx 38rpx;
+				.left {
+					float: left;
+				}
+				.right {
+					float: right;
+				}
+			}
+		}
+		.more {
+				   font-size: 20rpx;
+				   color: #7f7f7f;
+				   margin-top: 83rpx;
+				   margin-left: 220rpx;
+				   margin-bottom: 40rpx;
+		}
+		.last {
+				   width: 100%;
+				   height: 144rpx;
+				   padding: 55rpx 116rpx;
+				   background: linear-gradient(115deg, #6DD99B, #37AB63);
+				   font-size: 36rpx;
+				   font-weight: 900;
+				   color: #fff;
+				   display: flex;
+				   justify-content: space-around;
+				   align-items: center;
+		}
+	}
+</style>
+
+
