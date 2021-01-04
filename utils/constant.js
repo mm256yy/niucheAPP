2befc3674849c59e77b4df70dc258e20ebd9433e
pengtianfu <peng7800809@163.com>
2021-01-04 08:58:31 +0800

    优化

--- utils/constant.js
+++ utils/constant.js
@@ -8,4 +8,4 @@ export const actionJs = ipconfig+'/user/image/DriverCardLicense';
 export const baseUrl = ipconfig;
 export const shareUrl ="http://share.neocab.cn?shareId=" //分享
 export const updateUrl =ipconfig+"/app/getUrl?version=" 
-export const version = '1.5.1.9'
+export const version = '1.5.1.101'
