b491439c585cdf810b7e9ebca1b25146ae7d6347
pengtianfu <peng7800809@163.com>
2021-01-12 10:59:49 +0800

    复制 客服功能

--- pages/mycenter/company/service.vue
+++ pages/mycenter/company/service.vue
@@ -3,15 +3,15 @@
 		<u-navbar back-text="返回" back-icon-size="0" title="纽车客户服务中心" title-width="300" height='44' title-color="#111" title-bold="true">
 		</u-navbar>
 		<view style="width: 680rpx;margin-left: 36rpx;background: #fff;border-radius: 20rpx 20rpx 0 0;margin-top: 36rpx;">
-			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;border-bottom: 2rpx solid rgba(0,0,0,0.03);">
+<!-- 			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;border-bottom: 2rpx solid rgba(0,0,0,0.03);">
 				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">线上客服</view>
 				<view style="border-radius: 12rpx;border: 1px solid #4ABA75;line-height: 62rpx;text-align: center;
 				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;">联系客服</view>
-			</view>
+			</view> -->
 			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;border-bottom: 2rpx solid rgba(0,0,0,0.03);">
 				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">微信号：niuchekeji</view>
 				<view style="border-radius: 12rpx;border: 1px solid #4ABA75;line-height: 62rpx;text-align: center;
-				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;">复制加友</view>
+				font-size: 30rpx;color: #4aba75;margin-right: 36rpx;padding: 2rpx 16rpx;" @click="copyd()">复制加友</view>
 			</view>
 			<view style="display: flex;width: 680rpx;height: 140rpx;justify-content: space-between;align-items: center;">
 				<view style="margin-left: 36rpx;font-size: 30rpx;color: #333;">客服电话：0571-878115287</view>
@@ -39,6 +39,14 @@
 			dial(){
 				uni.makePhoneCall({
 				    phoneNumber: '0571-878115287' 
+				});
+			},
+			copyd(){
+				uni.setClipboardData({
+				    data: 'niuchekeji',
+				    success: function () {
+				        console.log('success');
+				    }
 				});
 			}
 	    },
