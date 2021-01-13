6ba797f64023c235e893c47fff927ddba02d8575
pengtianfu <peng7800809@163.com>
2021-01-13 16:12:18 +0800

    优化

--- pages/driver/cashAccount/cashWithdrawal.vue
+++ pages/driver/cashAccount/cashWithdrawal.vue
@@ -5,13 +5,13 @@
 			<view class="content_title">车辆月租金</view>
 			<view class="content_input">
 				<u-image src="@/static/money@3x.png" height="50rpx" width="44rpx" border-radius="8"></u-image>
-				<u-input v-model="form.rent" @input="rentMoney" type="number" maxlength="7" :border="false" placeholder="请输入当前车辆月租金" style="padding-left: 20px;" />
+				<u-input v-model="form.rent" @input="rentMoney" type="number" maxlength="7" :border="false" placeholder="请输入当前车辆月租金"/>
 			</view>
 			<view class="content_tips">*请输入与您租赁合同内一致的租金金额，否则会提现失败</view>
 			<view class="content_title">提现金额</view>
 			<view class="content_input">
 				<u-image src="@/static/money@3x.png" height="50rpx" width="44rpx" border-radius="8"></u-image>
-				<u-input v-model="form.money" @input="moneyInput" :border="false" type="number" maxlength="7" placeholder="请输入提现金额" style="padding-left: 20px;" />
+				<u-input v-model="form.money" @input="moneyInput" :border="false" type="number" maxlength="7" placeholder="请输入提现金额"/>
 			</view>
 			<view style="font-size: 12px;color:#C7C7C7 ;padding-top: 5px;">当前可提现金额<text>{{money}}</text>元</view>
 			<view class="content_tips" v-show="moneyTips">*{{moneyTipsText}}</view>
@@ -27,7 +27,7 @@
 			</view>
 			<view class="content_input" style="align-items: baseline;padding: 10px 0;">
 				<view style="color: #111111;font-weight: bold;">支付宝账号</view>
-				<u-input v-model="form.billingAccount" maxlength="100" :border="false" placeholder="请输入您的支付宝账户号" style="padding-left: 20px;" />
+				<u-input v-model="form.billingAccount" maxlength="100" :border="false" placeholder="请输入您的支付宝账户号"/>
 			</view>
 			<view class="content_tips">
 				*为保障您的合法权益，提现须上传车辆租赁合同和网约车平
