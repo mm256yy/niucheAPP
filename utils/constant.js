269043fc217a3770d8952f71f620600d28a8e6c8
pengtianfu <peng7800809@163.com>
2021-02-01 19:48:53 +0800

    常量加注释

--- utils/constant.js
+++ utils/constant.js
@@ -18,10 +18,13 @@ export const publishObj = {
 	errorType:['message'],
 	// navbar返回按钮样式
 	backTextStyle:{'color':'#ffffff'},
+	//navbar 标题的样式
 	titleStyle:{'color':'#111111','fontSize':'32rpx','fontWeight':500},
+	//开放城市
 	cityList:[{id: '0',text: '杭州',checked:false}],
-	// publicList:[{id: '1',text: '公开',checked:false},{id: '0',text: '不公开',checked:false}],
+	//发布状态
 	publiclist:[{id: '0',text: '不公开',checked:false},{id: '1',text: '公开',checked:false}],
+	//业务类型
 	onLineList:[{id: '1',text: '网约车',checked:false},{id: '2',text: '出租车',checked:false}],
 	//车辆类型
 	carType:[{id: '1',text: '轿车',checked:false},{id: '2',text: 'SUV',checked:false},{id: '3',text: 'MPV',checked:false},{id: '4',text: '其他',checked:false}],
@@ -30,19 +33,31 @@ export const publishObj = {
 	//日期
 	params: {year: true,month: true,day: false,hour: false,minute: false,second: false},
 	//租赁周期
-	leasePeriod: [{id: '1',text: '1个月',checked:false},{id: '2',text: '3个月',checked:false}, {id: '3',text: '6个月',checked:false}, {id: '4',text: '12个月',checked:false}],
-	ageList:[{id: '1',text: '不限',checked:false },{id: '2',text:'1年内',checked:false },{id: '3',text:'1年-3年',checked:false },{id: '4',text:'3年-5年',checked:false },{id: '5',text:'5年以上',checked:false }],
+	leasePeriod: [{id: '1',text: '1个月',checked:false},{id: '2',text: '3个月',checked:false}, {id: '3',text: '6个月',checked:false}, 
+	              {id: '4',text: '12个月',checked:false}],
+	//	车龄		  
+	ageList:[{id: '1',text: '不限',checked:false },{id: '2',text:'1年内',checked:false },{id: '3',text:'1年-3年',checked:false },
+	          {id: '4',text:'3年-5年',checked:false },{id: '5',text:'5年以上',checked:false }],
+	//	租金	  
 	rentList:[{id: '0',text:'3000以内（含3000）',checked:false },{id: '1',text:'3000以上',checked:false }],
 	objType:{
-		wycList:[{id: '',text:'不限',checked:false },{id: '0',text:'0-2万公里',checked:false },{id: '1',text:'2-5万公里',checked:false},{id: '2',text:'5-10万公里',checked:false },{id: '3',text:'10-20万公里',checked:false },
-		     {id: '4',text:'20-30万公里',checked:false },{id: '5',text:'30万公里以上',checked:false},],
-		      czcList:[{id: '',text:'不限',checked:false },{id: '0',text:'0-2万公里',checked:false },{id: '1',text:'2-5万公里',checked:false },
-		      {id: '2',text:'5-10万公里',checked:false },{id: '3',text:'10-20万公里',checked:false },{id: '4',text:'20-30万公里',checked:false },
-		      {id: '5',text:'30-50万公里',checked:false },{id: '6',text:'50-70万公里',checked:false },{id: '7',text:'70万公里以上',checked:false}],
-	},
-	salaryList:[{id: '0',text:'6000以内',checked:false },{id: '1',text:'6000-8000',checked:false },{id: '2',text:'8000-10000',checked:false },{id: '3',text:'10000以上',checked:false }],
-	hoursList:[{id: '0',text:'8小时',checked:false },{id: '1',text:'8-10小时',checked:false },{id: '2',text:'10小时以上',checked:false },{id: '3',text:'不限',checked:false }],
+		//网约车里程
+		wycList:[{id: '',text:'不限',checked:false },{id: '0',text:'0-2万公里',checked:false },{id: '1',text:'2-5万公里',checked:false},
+	            {id: '2',text:'5-10万公里',checked:false },{id: '3',text:'10-20万公里',checked:false },{id: '4',text:'20-30万公里',checked:false },
+	            {id: '5',text:'30万公里以上',checked:false},],
+		//出租车里程		
+	    czcList:[{id: '',text:'不限',checked:false },{id: '0',text:'0-2万公里',checked:false },{id: '1',text:'2-5万公里',checked:false },
+	             {id: '2',text:'5-10万公里',checked:false },{id: '3',text:'10-20万公里',checked:false },{id: '4',text:'20-30万公里',checked:false },
+	             {id: '5',text:'30-50万公里',checked:false },{id: '6',text:'50-70万公里',checked:false },{id: '7',text:'70万公里以上',checked:false}],
+		},
+	//月薪
+	salaryList:[{id: '0',text:'6000以内',checked:false },{id: '1',text:'6000-8000',checked:false },{id: '2',text:'8000-10000',checked:false },
+	            {id: '3',text:'10000以上',checked:false }],
+	//工作时长			
+	hoursList:[{id: '0',text:'8小时',checked:false },{id: '1',text:'8-10小时',checked:false },{id: '2',text:'10小时以上',checked:false },
+	           {id: '3',text:'不限',checked:false }],
+	//福利要求		   
 	benefitList:[{id:'0',text: '有3险',checked: false},{id:'1',text: '有5险',checked: false},{id:'2',text: '无责任底薪',checked: false},
-			{id:'3',text: '月休4天',checked: false}]
+			    {id:'3',text: '月休4天',checked: false}]
 	
 }
