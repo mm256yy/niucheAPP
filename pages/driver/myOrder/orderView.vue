ad092e09199847a4f3ef035ee0d052aab8724cca
pengtianfu <peng7800809@163.com>
2021-02-22 18:06:04 +0800

    订单

---
+++ pages/driver/myOrder/orderView.vue
@@ -0,0 +1,122 @@
+<template>
+	<view>
+		<u-navbar back-icon-color="#111111" title="订单详情" :background="background" title-color="#111111">
+			<view slot="" @click="refreshView">刷新</view>
+			<view class="navbar-right" slot="right">
+				<view class="message-box right-item" @click="chargeback" v-if="status">退单</view>
+			</view>
+		</u-navbar>
+		<view class="content">
+			<view class="count_down">剩余<u-count-down :timestamp="timestamp" color="#FE5B00" separator-color="#FE5B00" @end="countEnd"></u-count-down>
+			</view>
+			<view style="margin:16rpx 80rpx 0;display: flex;justify-content:center;width: 90%;">
+				<u-time-line>
+					<u-time-line-item nodeTop="2">
+						<!-- 此处自定义了左边内容，用一个图标替代 -->
+						<template v-slot:node>
+							<view class="u-node">
+								<!-- 此处为uView的icon组件 -->
+								<view class="time">
+									<view>2020-09-28</view>
+									<view style="font-size: 24rpx;text-align: center;">11:15:33</view>
+								</view>
+								<u-image width="52rpx" height="52rpx" src="@/static/qianyue@2x.png"></u-image>
+							</view>
+						</template>
+						<template v-slot:content>
+							<view style="height: 52px;">
+								<view class="u-order-title">完成</view>
+							</view>
+						</template>
+					</u-time-line-item>
+					<u-time-line-item>
+						<!-- 此处没有自定义左边的内容，会默认显示一个点 -->
+						<template v-slot:content>
+							<view>
+								<view class="u-order-desc">提车</view>
+							</view>
+						</template>
+					</u-time-line-item>
+				</u-time-line>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				background: {
+					'background-image': 'linear-gradient(to bottom, #000000 39%,#ffffff 0%)'
+				},
+				timestamp: 86399,
+				status: true, //状态
+			}
+		},
+		methods: {
+			refreshView() {
+				console.log(222)
+			},
+			chargeback() {
+				console.log('退单')
+			},
+			countEnd() {
+				console.log(2221)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.navbar-right {
+		margin-right: 24rpx;
+		display: flex;
+	}
+
+	.right-item {
+		margin: 0 12rpx;
+		position: relative;
+		color: #333333;
+		display: flex;
+	}
+
+	.content {
+
+		.count_down {
+			text-align: center;
+			padding: 40rpx 0 20rpx;
+			color: #FE5B00;
+		}
+
+		.u-node {
+			width: 52rpx;
+			height: 52rpx;
+			border-radius: 100rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background: #d0d0d0;
+			.time{
+				position: absolute;left: -100px;color: #000000;font-size: 28rpx;
+			}
+		}
+         
+		.u-order-title {
+			color: #333333;
+			font-weight: bold;
+			font-size: 32rpx;
+		}
+
+		.u-order-desc {
+			color: rgb(150, 150, 150);
+			font-size: 28rpx;
+			margin-bottom: 6rpx;
+		}
+
+		.u-order-time {
+			color: rgb(200, 200, 200);
+			font-size: 26rpx;
+		}
+	}
+</style>
--- pages/driver/onlineCar/onlineCar.vue
+++ pages/driver/onlineCar/onlineCar.vue
@@ -1,11 +1,6 @@
 <template>
 	<view>
 		<u-navbar back-text="返回"  back-icon-size="0" :title="title" :back-text-style="backTextStyle" :background="backgroundDri" title-color="#FFFFFF">
-			<view class="navbar-right" slot="right">
-				<view class="message-box right-item">
-					<text v-show="stateType && !type" @click="edit">变更</text>
-				</view>
-			</view>
 		 </u-navbar>
 		<view class="top-content" >
 			<view style="background-color: #dedede;padding: 8pt 0;" v-show="reason">
@@ -26,42 +21,7 @@
 			<view class="" v-else>
 				<u-image width="100%" height="240rpx" :src="form.driverPhoto"></u-image>
 			</view>
-			<view class="top-content-uploadTips" style="padding:10pt 0 5pt;">1.必须为jpg格式,单张不得超过4M</view>
-			<view class="top-content-uploadTips">2.上传后自动或手动识别文字信息</view>
-		</view>
-		<view class="middle-content">
-			<u-form :model="form" ref="uForm" label-width="220" :error-type="errorType" :border-bottom="false">
-				<u-form-item label="姓名" prop="name">
-					<u-input class="input-radius" v-model="form.name" :border="true" v-if="type"/>
-					<view v-else class="type-right">{{form.name}}</view>
-				</u-form-item>
-				<u-form-item label="性别" prop="sex">
-					<u-radio-group v-if="type" v-model="form.sex" :active-color="'#FFA032'" style="text-align: right;">
-						<u-radio :name="item.name" style="margin-left: 10pt;" v-for="item in radioList" :key="item.name">{{item.text}}</u-radio>
-					</u-radio-group>
-					<view class="type-right" v-else>{{form.sex === '0' ? "女":"男"}}</view>
-				</u-form-item>
-				<u-form-item label="证号" prop="licenseNumber">
-					<u-input class="input-radius" v-model="form.licenseNumber" maxlength="18" :border="true" v-if="type"/>
-					<view class="type-right" v-else>{{form.licenseNumber}}</view>
-				</u-form-item>
-				<u-form-item label="初次领证" prop="username">
-					<u-input v-model="form.issueDate" :border="true" class="input-radius" :disabled="true"
-					 @click="showPicker('issueDate')" placeholder="" v-show='type'/>
-					<u-icon class="iconAbs" name="calendar" color="#FFA032" size="40" v-show='type'></u-icon>	
-					<view class="type-right" v-show='!type'>{{form.issueDate}}</view>
-				</u-form-item>
-				<u-form-item label="有效起始时间" prop="beginTime">
-					<u-input v-model="form.beginTime" :border="true" v-show='type'
-					 class="input-radius" :disabled="true" @click="showPicker('beginTime')" placeholder=""/>
-					<u-icon class="iconAbs" v-show='type' name="calendar" color="#FFA032" size="40"></u-icon>
-					<view class="type-right" v-show='!type'>{{form.beginTime}}</view>
-				</u-form-item>
-			</u-form>
-			<view style="padding-top: 30pt;color:#E10000;">
-				*以上项目都是必填项，未完成不能提交！ 
-			</view>
-			<u-picker v-model="show" mode="time" :end-year="today.year" :params="params" @confirm="dataChange"></u-picker>
+			<view class="top-content-uploadTips" style="padding:10pt 0 5pt;">1.必须为jpg格式,单张不得超过8M</view>
 		</view>
 		<view style="text-align: center; padding: 5pt 30pt;" v-show="type">
 			<u-button type="warning" shape='circle' class="btn-orange" @click="toNext">提交</u-button>
@@ -84,76 +44,34 @@
 				backTextStyle:{'color':'#ffffff'},
 				title:'证件上传',
 				action: action,
-				headerObj:{Authorization:''},
-				formDataObj:{phone:''},
 				fileList: [],
-				radioList:[{name:'0',text:'女'},{name:'1',text:'男'}],
 				form: {
 					type:1,//网约车1 出租车2 驾照0
-					name:'',
-					sex: '',
-					licenseNumber:'',
-					issueDate:'',
-					beginTime:'',
-					vehicleAge:'',
 					driverPhoto:''
 				},
-				rules: {
-					name:requiredRule,
-					sex:requiredRule,
-					licenseNumber:requiredRule,
-					issueDate:requiredRule,
-					beginTime:requiredRule,
-                },
-				pickerName:'',
-				List:[{text:'A1'},{text:'A2'},{text:'A3'},{text:'B1'},{text:'B2'},{text:'C1'},{text:'C2'}],
 				show: false,
-				showEnd:false,
 				showTips:false,
 				type:true,//切换修改
 				stateType:false,
 				reason:'',
-				params: {
-					year: true,month: true,day: true,hour: false,minute: false,second: false
-				},
 				id:'',
 				errorType:[
 					'message'
 				],
-				today:{}
 			}
-		},
-		onReady() {
-		    this.$refs.uForm.setRules(this.rules);
 		},
 		onBackPress(event) {
 			 this.$u.api.setEvent({eventId:"attestation_Return_1",type:3,params:{souce:'网约车认证'}})
 		},
 		mounted() {
-            let today = uni.getStorageSync('today');
-			if(today){
-				this.today = today
-			} else{
-				this.initDate()
-			}
 			this.getInfo()
 		},
 		methods: {
-			initDate(){
-				let date = new Date();
-				let year = date.getFullYear();
-				let month = date.getMonth()+1;
-				let day = date.getDate();
-				let obj = {year:year,month:month,day:day};
-				uni.setStorage('today',obj)
-				this.today = obj;
-			},
 			getInfo(){
 				this.$u.api.listDrivingLicense({state:1}).then(res => {
 					if(res.code === 200){
 					let data = res.object;
 						this.form =data;
-						this.form.sex = data.sex.toString();
 						this.form.type = 1;
 					this.fileList= [{url:data.driverPhoto}]
 						if (data.certificationstate === 3){
@@ -181,9 +99,6 @@
 			confirm(){
 				this.$u.route({url:'/pages/mycenter/mycenter',type:'switchTab'})
 			},
-			edit(){
-				this.type = true;
-			},
 			toNext(){
 				this.$refs.uForm.validate(valid=>{
 					if(valid) {
@@ -196,10 +111,6 @@
 						}).catch(res=>{this.$u.toast(res.msg)})
 					} 
 				})
-			},
-			dataChange(obj){
-				let companyDate = obj.year+"-"+obj.month+"-"+obj.day;
-				this.form[this.pickerName] = companyDate;
 			},
 			uploadChange(res,index,lists,name){
 				this.form.driverPhoto = res.object
