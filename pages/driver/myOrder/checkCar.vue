c65fe9433e7a1f9698e054d9ea04bad2f016db57
pengtianfu <peng7800809@163.com>
2021-02-24 10:09:47 +0800

    mixin 导航栏背景色

---
+++ pages/driver/myOrder/checkCar.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<u-navbar back-icon-color="#111111" title="验车单(1/2)" :background="background" title-color="#111111"></u-navbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
